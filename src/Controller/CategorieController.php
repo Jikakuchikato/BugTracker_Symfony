a36a405e97ce82b9b2a2ee1aaa99ee0eab25e5a5
Jikakuchikato <gaetan.damore@gmail.com>
2023-01-01 14:08:25 +0100

    V2.5.2 Ajout modifications catégories

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -71,6 +71,39 @@ class CategorieController extends AbstractController
             'createCategorieForm' => $form->createView(),
         ]);
     }
+    #[Route('/categorie/modif', name: 'app_modif_categorie')]
+    public function categorieModif(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $categorie = $entityManager->getRepository(Categorie::class)->findOneBy(array("id" => $_GET['catId']));
+
+        $form = $this->createForm(CreateCategorieFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $categorie->setTitre($form->get('titre')->getData());
+            $categorie->setDescription($form->get('description')->getData());
+
+            $entityManager->flush();
+
+            $projet = $entityManager->getRepository(Projet::class)->findOneBy(array('id'=>$_GET['projetId']));
+            $categories = $entityManager->getRepository(Categorie::class)->findBy(array('projetId' => $_GET['projetId']));
+
+            return $this->render('projets/categories.html.twig', [
+                'infos' => 'Catégorie modifié avec succès.',
+                'categories' => $categories,
+                'projet' => $projet,
+            ]);
+        }
+
+        return $this->render('categorie/modifCategorie.html.twig', [
+            'categorie' => $categorie,
+            'createCategorieForm' => $form->createView(),
+        ]);
+        
+    }
 
     #[Route('/categorie/delete', name: 'app_categorie_delete')]
     public function categorieDelete(EntityManagerInterface $entityManager): Response
